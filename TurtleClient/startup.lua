1af12c38e51d4b9c27960386d6debcd2b02aafb9
4loneWolf <100577038+4loneWolf@users.noreply.github.com>
2022-07-04 22:54:57 +0700

    Add files via upload

--- TurtleClient/startup.lua
+++ TurtleClient/startup.lua
@@ -1,11 +1,10 @@
 local NameHeader = "?myName="
-local CordsHeader = ""
 local link = "ws://localhost:34197"
 local x = "none"
 local y = "none" 
 local z = "none"
-if fs.exists("cords") == true then    
-else
+
+if fs.exists("cords") == false then
     io.write('Write turtles exact coordinates \n')
     io.write('x: ')
     x = io.read("*l")
@@ -15,10 +14,12 @@ else
     z = io.read("*l")
     fs.open("cords", "w")
 end
+
 local name = "NoName"
-if fs.exists("name") == true then 
+if fs.exists("name") == true then
     local file = fs.open("name", "r")
     local name = file.readAll()
+    os.setComputerLabel(name)
     file.close()
     a = http.websocket(link .. NameHeader .. name .. "&" .. "x=" .. x .. "&" .. "y=" .. y .. "&" .. "z=" .. z)
 else 
@@ -28,8 +29,28 @@ else
     file.writeLine(name)
     if name == nil then
         fs.delete('name')
+        fs.delete('cords')
     end
+    os.setComputerLabel(name)
     print(name)
-end 
+end
+
+--while true do
+    --message = a.receive()
+    --message = load("return "..message.."")() -- LUA TABLE STRING TO AN ACTUAL LUA TABLE 
+    --print(message.message)
+    --print(message.zalupa)
+--end
+
+--while true do
+    --io.write('Enter your message here: ')
+    --local message = io.read('*l')
+    --a.send(message)
+--end
+
+--local array = {huesos = "hehehehehe", dfwefwe = "fefwefxafcdas"}
+--a.send(array)
+
+
 
 
